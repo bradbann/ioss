fc29bdd9e8816ab2faf7962834f372e22f00a7c6
SandersXin <1984019133@qq.com>
2016-07-20 16:23:02 +0800

    登录、主页和搜索界面静态页面

---
+++ portal/src/main/java/com/ggk/ioss/portal/web/contorller/MainController.java
@@ -0,0 +1,19 @@
+package com.ggk.ioss.portal.web.contorller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class MainController{
+	
+	@RequestMapping(value = "/")
+    public ModelAndView index(HttpServletRequest request, HttpServletResponse response) {
+        ModelAndView result = new ModelAndView("/html/searchInfo.html");
+        return result;
+    }
+	
+}
