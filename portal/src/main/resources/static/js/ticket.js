add44148951da580da4c1e8c66b0fddf761ea28f
ChengchuangLin <lincc@ggkbigdata.com>
2016-09-22 08:28:49 +0800

    3rd deploy bugfix

--- portal/src/main/resources/static/js/ticket.js
+++ portal/src/main/resources/static/js/ticket.js
@@ -1,56 +1,53 @@
 var thisURL = document.URL; 
-	var  param =thisURL.split('?')[1];
-	var paramVal= param.split("=")[1];
-	
-	if("" != paramVal){
-		$.ajax({
-			url: "/ioss/knowledge/ticket?eventId="+paramVal,
+    var  param =thisURL.split('?')[1];
+    var paramVal= param.split("=")[1];
+    
+    if("" != paramVal){
+        $.ajax({
+            url: "/ioss/knowledge/ticket?eventId="+paramVal,
             type: "GET",
             data: {},
             dataType: "json",
             success: function(data){
-            	if(data){
-            		var str = data;
-            		console.log(str);
-            		var regexstr = new RegExp("<[^<]*>", "gi");
-//            		$('#title').text(data["eventTitle"].replace(regexstr,""));
-            		$('#reportArea').text(data.data["reportArea"]);
-            		$('#reportor').text(data.data["reportor"]);
-            		$('#commitTime').text(data.data["commitTime"]);
-            		$('#updateTime').text(data.data["updateTime"]);
-            		$('#createTime').text(data.data["createTime"]);
-            		$('#eventId').text(data.data["eventId"]);
-            		$('#eventTitle').text(data.data["eventTitle"]);
-            		$('#eventDescr').text(data.data["eventDescr"]);
-            		$('#eventType').text(data.data["eventType"]);
-            		$('#eventDescr').text(data.data["eventDescr"]);
-            		$('#eventClassify').text(data.data["eventClassify"]);
-            		$('#ownerSystem').text(data.data["ownerSystem"]);
-            		$('#ownerModule').text(data.data["ownerModule"]);
-            		$('#subModule').text(data.data["subModule"]);
-            		$('#funcMenu').text(data.data["funcMenu"]);
-            		$('#eventDescr').text(data.data["eventDescr"]);
-            		$('#affectRange').text(data.data["affectRange"]);
-            		$('#affectDegree').text(data.data["affectDegree"]);
-            		$('#criticalDegree').text(data.data["criticalDegree"]);
-            		$('#pri').text(data.data["pri"]);
-            		$('#eventSource').text(data.data["eventSource"]);
-            		$('#eventStatus').text(data.data["eventStatus"]);
-            		$('#statusReason').text(data.data["statusReason"]);
-            		$('#solveCode').text(data.data["solveCode"]);
-            		$('#closeCode').text(data.data["closeCode"]);
-            		$('#currentDealGroup').text(data.data["currentDealGroup"]);
-            		$('#currentDealor').text(data.data["currentDealor"]);
-            		$('#solution').text(data.data["solution"]);
-            	}
+                if(data){
+                    var str = data;
+                    var regexstr = new RegExp("<[^<]*>", "gi");
+//                    $('#title').text(data["eventTitle"].replace(regexstr,""));
+                    $('#reportArea').text(data.data["reportArea"]);
+                    $('#reportor').text(data.data["reportor"]);
+                    $('#commitTime').text(data.data["commitTime"]);
+                    $('#updateTime').text(data.data["updateTime"]);
+                    $('#createTime').text(data.data["createTime"]);
+                    $('#eventId').text(data.data["eventId"]);
+                    $('#eventTitle').text(data.data["eventTitle"]);
+                    $('#eventDescr').text(data.data["eventDescr"].replace(regexstr,""));
+                    $('#eventType').text(data.data["eventType"]);
+                    $('#eventClassify').text(data.data["eventClassify"]);
+                    $('#ownerSystem').text(data.data["ownerSystem"]);
+                    $('#ownerModule').text(data.data["ownerModule"]);
+                    $('#subModule').text(data.data["subModule"]);
+                    $('#funcMenu').text(data.data["funcMenu"]);
+                    $('#affectRange').text(data.data["affectRange"]);
+                    $('#affectDegree').text(data.data["affectDegree"]);
+                    $('#criticalDegree').text(data.data["criticalDegree"]);
+                    $('#pri').text(data.data["pri"]);
+                    $('#eventSource').text(data.data["eventSource"]);
+                    $('#eventStatus').text(data.data["eventStatus"]);
+                    $('#statusReason').text(data.data["statusReason"]);
+                    $('#solveCode').text(data.data["solveCode"]);
+                    $('#closeCode').text(data.data["closeCode"]);
+                    $('#currentDealGroup').text(data.data["currentDealGroup"]);
+                    $('#currentDealor').text(data.data["currentDealor"]);
+                    $('#solution').text(data.data["solution"].replace(regexstr,""));
+                }
              }
         });
-	}
-	
+    }
+    
 $(function(){
-	//点击搜索服务连接
-	$('#searchSever').click(function(){
-		var url = "/html/search_result.html?content=";
-		window.open(url);
-	});
+    //点击搜索服务连接
+    $('#searchSever').click(function(){
+        var url = "/html/search_result.html?content=";
+        window.open(url);
+    });
 })
